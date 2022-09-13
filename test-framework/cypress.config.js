d54d3e25e7681b91d08120cfa57ee39f02931f72
testerspl <110538354+testerspl@users.noreply.github.com>
2022-09-13 11:40:42 +0200

    *

--- test-framework/cypress.config.js
+++ test-framework/cypress.config.js
@@ -27,7 +27,7 @@ module.exports = defineConfig({
 	fixturesFolder: 'cypress/fixtures',
 	e2e: {
 		setupNodeEvents(on, config) {},
-		baseUrl: 'http://localhost:8888',
+		baseUrl: 'http://localhost',
 		excludeSpecPattern: 'testTest.spec.js',
 		specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
 		supportFile: './cypress/support/e2e.js',
