c2c1c73890e8ca2e9326b0a79aa3042f990ecc33
testerspl <110538354+testerspl@users.noreply.github.com>
2022-09-13 09:19:46 +0200

    *

--- app-version-one/js/footer.jsx
+++ app-version-one/js/footer.jsx
@@ -42,7 +42,7 @@ var app = app || {};
 						{' '}
 						<li>
 							<a
-								href="#/active" data-test={"filter-activeTodos"}
+								href="#/active"
 								className={cx({selected: nowShowing === app.ACTIVE_TODOS})}>
 									Active
 							</a>
