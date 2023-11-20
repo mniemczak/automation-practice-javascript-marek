7158b327b28e68ffe944ee36f779ad520d57cc86
testerspl <110538354+testerspl@users.noreply.github.com>
2023-11-20 11:17:19 +0100

    Wstępne informacje o zmiennych

---
+++ test-framework/example.js
@@ -0,0 +1,22 @@
+
+
+// W języku JAVA // String myName = "Krzysztof"
+// var
+let myName
+const mynewName = "Krzysztof"
+
+
+myName = "Krzysztof"
+myName = 2
+myName = []
+myName = {}
+myName = 2
+myName = 'Krzysztof'
+
+// Brak możliwości zmiany w ten sposób // mynewName = "Marek"
+
+let secondName = mynewName.replaceAll('Krzysztof', 'Marek')
+
+
+
+console.log(typeof mynewName)
