0a803b682eeaf51214e063158741dfeb70224294
mniemczak <83756677+mniemczak@users.noreply.github.com>
2025-03-12 10:43:14 +0000

    Dodany plik zadania.js zawierający rozwiązania zadań z modułu 2

---
+++ zadania.js
@@ -0,0 +1,32 @@
+//Rozwiązanie do zadania 1a
+
+class Klasa{
+    metodaA(name){
+        console.log(name);
+        return name;
+    }
+
+    metodaB(name){
+        const name2 = this.metodaA(name);
+        
+        //wylogowanie 2 pierwszych liter imienia
+        console.log(name2.slice(0,2));
+        
+        //wylogowanie imienia z małej litery
+        console.log(name2.charAt(0).toLowerCase() + name2.slice(1));
+
+        //wylogowanie imienia z dużej litery
+        console.log(name2.charAt(0).toUpperCase() + name2.slice(1));
+
+        //wylogowanie imienia z nazwiskiem + 2 dodatkowe słowa
+        console.log(`${name2}` + ` Kowalski` + ` Tester` + ` oprogramowania`);
+    }
+}
+
+//Kilkukrotne wywołanie metodaA z różnymi imionami:
+const klasa1 = new Klasa();
+klasa1.metodaA("Krzysztof");
+klasa1.metodaA("Ania");
+klasa1.metodaA("Radek");
+
+klasa1.metodaB("Marek");
