5fa1101c33280885432a6e2b8dfe9a5ee8c3e0c3
mniemczak <83756677+mniemczak@users.noreply.github.com>
2025-03-24 20:09:09 +0100

    zadanie extra

---
+++ zadanie-domowe-extra.js
@@ -0,0 +1,59 @@
+class User{
+    constructor(name, phoneNumber, email, age, address){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.age = age;
+        this.address = address;
+    }
+    // Metoda logująca dane użytkownika
+    logUserData() {
+        console.log(`Imię: ${this.name}, Numer telefonu: ${this.phoneNumber}, E-mail: ${this.email}`);
+    }
+
+    // Metoda sprawdzająca wiek użytkownika
+    checkAge() {
+        if (this.age >= 18) {
+            console.log(`${this.name} jest pełnoletni.`);
+        } else {
+            console.log(`${this.name} nie jest pełnoletni.`);
+        }
+    }
+
+    // Metoda wykorzystująca wiele elementów z konstruktora
+    displayFullInfo() {
+        console.log(`Imię: ${this.name}, Numer telefonu: ${this.phoneNumber}, E-mail: ${this.email}, Wiek: ${this.age}, Adres: ${this.address}`);
+    }
+}
+
+class BasicUser extends User {
+    constructor() {
+        super("Jan Kowalski", "123456789", "jan@example.com", 25, "Warszawa");
+    }
+
+    // Metoda wykorzystująca metody z klasy bazowej
+    getUserData() {
+        this.logUserData();
+        this.checkAge();
+        this.displayFullInfo();
+    }
+}
+
+class CartUser extends User {
+    constructor() {
+        super("Anna Nowak", "987654321", "anna@example.com", 30, "Kraków");
+    }
+
+    // Metoda wykorzystująca metody z klasy bazowej
+    getUserData() {
+        this.logUserData();
+        this.checkAge();
+        this.displayFullInfo();
+    }
+}
+
+const basicUser = new BasicUser();
+basicUser.getUserData();
+
+const cartUser = new CartUser();
+cartUser.getUserData();
